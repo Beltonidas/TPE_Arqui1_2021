cf2736e654cf2ed4b7042a9cd0c3d8d986916d3a
InakiArzalluz <inoooaak@gmail.com>
2021-06-12 12:45:54 -0300

    Finalización de codigo y adición de diagramas en carpeta _Complemento

--- MIPS Segmentado.srcs/sources_1/imports/LabMaterial/processor.vhd
+++ MIPS Segmentado.srcs/sources_1/imports/LabMaterial/processor.vhd
@@ -50,7 +50,7 @@ end component;
    signal EX_RegDst, EX_ALUSrc, EX_MemtoReg, EX_RegWrite, EX_MemRead, EX_MemWrite, EX_Branch:std_logic;--Registro de segmentacion
    signal EX_AluOp:std_logic_vector(2 downto 0); --Registro de segmentacion
    signal EX_Reg_Destino0,  EX_Reg_Destino1:std_logic_vector (4 downto 0); --Registro de segmentacion
-   signal EX_sign_ext, EX_data2_rd_rg, EX_data1_rd_rg, EX_PC_4 : std_logic_vector (31 downto 0); --Registro de segmentacion
+   signal EX_sign_ext, EX_data2_rd_rg, EX_data1_rd_rg, EX_PC_4: std_logic_vector (31 downto 0); --Registro de segmentacion
       
    signal RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch: std_logic;
    signal AluOp: std_logic_vector(2 downto 0);
@@ -58,12 +58,34 @@ end component;
 ---------Fin Sección Instruction Decode---------
 
 ---------Inicio sección Execution--------------
-------------fin sección Execution--------------
+component P1c is port( --ALU
+            a : in std_logic_vector (31 downto 0);
+            b : in std_logic_vector (31 downto 0);
+            control: in std_logic_vector (2 downto 0);
+            output: out std_logic_vector (31 downto 0);
+            zero : out std_logic  
+        );
+end component;
 
+    signal ALU_b,ALU_result, Add_result: std_logic_vector (31 downto 0);
+    signal reg_destino:std_logic_vector (4 downto 0);
+    signal ALU_zero: std_logic;
+    signal ALU_ctrl: std_logic_vector(2 downto 0);
+    signal funct: std_logic_vector(5 downto 0);
+    signal MEM_RegWrite, MEM_MemtoReg, MEM_Branch, MEM_MemWrite, MEM_MemRead, MEM_ALU_zero:std_logic;
+    signal MEM_Add_result, MEM_ALU_result, MEM_data2_rd_rg:std_logic_vector(31 downto 0);
+    signal MEM_reg_destino: std_logic_vector (4 downto 0);
+------------fin sección Execution--------------
+------------inicio sección Memory--------------
+    signal mem_reg_data: std_logic_vector (31 downto 0);
+------------fin sección Memory--------------
 ---------Inicio sección Write Back--------------
-    signal WB_RegWrite: std_logic;
     signal dato_reg_destino_wb: std_logic_vector(31 downto 0);
     signal reg_Destino_wb: std_logic_vector(4 downto 0);
+    signal WB_RegWrite, WB_MemtoReg: std_logic;
+    signal WB_mem_reg_data, WB_ALU_result: std_logic_vector(31 downto 0);
+    signal WB_reg_destino: std_logic_vector (4 downto 0);
+    
 ------------fin sección Write Back--------------
 begin 	
 Reg_bank: p1d port map( --Connexión del banco de registros
@@ -72,15 +94,26 @@ Reg_bank: p1d port map( --Connexión del banco de registros
         wr  => WB_RegWrite, --ID_PC_4
         reg1_rd => reg1_rd_rg, --reg1_rd_rg <= Instruction(25 downto 21);
         reg2_rd => reg2_rd_rg,
-        reg_wr  => reg_wr_rg,    --ACA PODRIA CONECTAR DIRECTO DEL WB?
-        data_wr => data_wr_rg,   --ACA PODRIA CONECTAR DIRECTO DEL WB?
+        reg_wr  => WB_reg_destino,
+        data_wr => dato_reg_destino_wb,   --¿ACA PODRIA CONECTAR DIRECTO DEL WB?
         data1_rd => data1_rd_rg, 
         data2_rd => data2_rd_rg
     );
+
+ALU: P1c port map( --Connexión de la ALU
+         a => EX_data1_rd_rg,
+         b => ALU_b,
+         control => ALU_ctrl,
+         output => ALU_result,  
+         zero => ALU_zero
+    );    
+
+ALU_b <= EX_data2_rd_rg when EX_ALUSrc='0' else
+        EX_sign_ext;    
     
 --Multiplexor que elige instrucción a meter en Program Conter
 PC_next <= PC_4 when PCSrc='0' else
-          PC_Branch;
+          MEM_Add_result;
 
 --Definicion del Program Counter (PC) (Es un registro)
 PC_Process:    process(Clk,Reset)
@@ -98,6 +131,8 @@ PC_4 <= PC+4;
 -------------------------------------------------------------------------------------------------------------------------
 --CONFIRMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAR.
 --Manejo de I_Addr (Este puerto despues se conecta a la memoria en el TB)
+I_WrStb <= '0';
+I_RdStb <= '1'; --¿Esto es correcto para que siempre lea?
 I_Addr <= PC;
 --CONFIRMAR, porque estoy escribiendo directamene a un puerto del procesador.
 --CONFIRMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAR.
@@ -127,8 +162,7 @@ reg2_rd_rg <= Instruction(20 downto 16);
 --data2_rd_rg <= reg2_rd_rg; --Leemos los registros que anteriormente seleciconamos 
 
 --Definimos registro de escritura final
-reg_wr_rg <= reg_Destino_wb;
-data_wr_rg <= dato_reg_destino_wb;
+--data_wr_rg <= dato_reg_destino_wb;
 
 --Estos 2 los valores a elegir en el mux de la etapa EX
 Reg_destino0 <= Instruction(20 downto 16);
@@ -137,13 +171,13 @@ Reg_destino1 <= Instruction(15 downto 11);
 --Definición de Unidad de extensión de signo.
 process (Instruction)  
     --Confirmar lista de sensibilidad y no haber extendido al reves, o tomado los bits que no eran (31 a 15) o (15 a 0)
-begin
-    if (Instruction(15) = '1') then
-    sign_ext <=  x"FFFF" & Instruction(15 downto 0);
-    else
-    sign_ext <=  x"0000" & Instruction(15 downto 0);
-    end if;
-end process;
+    begin
+        if (Instruction(15) = '1') then
+        sign_ext <=  x"FFFF" & Instruction(15 downto 0);
+        else
+        sign_ext <=  x"0000" & Instruction(15 downto 0);
+        end if;
+    end process;
 --CONFIRMAR QUE NO HAYA HECHO COSAS AL REVES
 
 --Definición de Unidad de control, todo va a parar al registro de segmentación (eventualmente)
@@ -272,4 +306,92 @@ reg_ID_EX:  process(Clk,Reset)
 -------------------------------------------------------------------------------------------------------------------------
 --ID/EX
 -------------------------------------------------------------------------------------------------------------------------             
+-- Definición de Unidad ALU Control
+process (EX_sign_ext,EX_AluOp)
+begin
+    funct <= EX_sign_ext(5 downto 0);
+    case EX_AluOp is
+       when "000" => ALU_ctrl <= "010";
+       when "001" => ALU_ctrl <= "110";
+       when "011" => ALU_ctrl <= "100";
+       when "100" => ALU_ctrl <= "000";
+       when "101" => ALU_ctrl <= "001";
+       when "010" => 
+            case funct is
+                when "100000" => ALU_ctrl <="010";
+                when "100010" => ALU_ctrl <="110";
+                when "100100" => ALU_ctrl <="010";
+                when "100101" => ALU_ctrl <="001";
+                when "101010" => ALU_ctrl <="111";
+            end case;
+    end case;
+end process;
+
+reg_destino <= EX_Reg_destino0  when EX_RegDst='0' else
+               EX_Reg_destino1;
+               
+Add_result <= EX_PC_4 + (EX_sign_ext(29 downto 0) & "00");
+
+--Definición de registro de segmentación EX/MEM
+reg_EX_MEM:      process(Clk,Reset) 
+                begin
+                    if(Reset = '1') then
+                        MEM_Add_result <= (others => '0');
+                        MEM_ALU_result <= (others => '0');
+                        MEM_data2_rd_rg <= (others => '0');
+                        MEM_reg_destino <= (others => '0');
+                        MEM_RegWrite <= '0';
+                        MEM_MemtoReg <= '0';
+                        MEM_Branch <= '0';
+                        MEM_MemWrite <= '0';
+                        MEM_MemRead  <= '0';
+                        MEM_ALU_zero <= '0';
+                    elsif  (rising_edge(Clk)) then
+                        MEM_Add_result <= Add_result ;
+                        MEM_ALU_result <= ALU_result;
+                        MEM_data2_rd_rg <= EX_data2_rd_rg;
+                        MEM_reg_destino <= reg_destino;
+                        MEM_RegWrite <= EX_RegWrite;
+                        MEM_MemtoReg <= EX_MemtoReg;
+                        MEM_Branch <= EX_Branch;
+                        MEM_MemWrite <= EX_MemWrite;
+                        MEM_MemRead  <= EX_MemRead;
+                        MEM_ALU_zero <= ALU_zero;
+                    end if;
+                end process;
+-------------------------------------------------------------------------------------------------------------------------
+--EX/MEM
+-------------------------------------------------------------------------------------------------------------------------             
+PCSrc <= MEM_Branch and MEM_ALU_zero;
+
+--Lectura de memoria externa
+D_Addr <= MEM_ALU_result;
+D_DataOut <= MEM_data2_rd_rg;
+D_RdStb <= MEM_MemRead;
+D_WrStb <= MEM_MemWrite;
+mem_reg_data <= D_DataIn;
+
+--Definición de registro de segmentación MEM/WB
+reg_MEM_WB:      process(Clk,Reset) 
+                begin
+                    if(Reset = '1') then
+                        WB_RegWrite <= '0';
+                        WB_MemtoReg <= '0';
+                        WB_mem_reg_data <= (others => '0');
+                        WB_ALU_result <= (others => '0');
+                        WB_reg_destino <= (others => '0');
+                    elsif  (rising_edge(Clk)) then
+                        WB_RegWrite <= MEM_RegWrite;
+                        WB_MemtoReg <= MEM_MemtoReg;
+                        WB_mem_reg_data <= mem_reg_data;
+                        WB_ALU_result <= MEM_ALU_result;
+                        WB_reg_destino <= MEM_reg_destino;
+                    end if;
+                end process;
+-------------------------------------------------------------------------------------------------------------------------
+--MEM/WB
+-------------------------------------------------------------------------------------------------------------------------
+dato_reg_destino_wb <= WB_mem_reg_data when WB_MemtoReg='0' else
+                       WB_ALU_result;
+
 end processor_arq;
